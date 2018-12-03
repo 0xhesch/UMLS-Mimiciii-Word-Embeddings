d1959b0ab01067a9612e4ce745abfb7b3f649a1c
0xhesch <37747481+0xhesch@users.noreply.github.com>
2018-12-03 11:54:32 +0100

    Update extract_to_fasttext.py

--- fastText/extract_to_fasttext.py
+++ fastText/extract_to_fasttext.py
@@ -1,10 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
 
-"""
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 Henning S.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import psycopg2
 
-conn=psycopg2.connect("dbname='mimic' user='postgres' host='localhost' password='root'")
+#connection string
+conn=psycopg2.connect("dbname='mimic' user='postgres' host='localhost' password='xx'")
 
 cur = conn.cursor()
 cur.execute("""SELECT text, hadm_id FROM mimiciii.noteevents WHERE category='Discharge summary'""")
@@ -17,4 +30,4 @@ for entry in raw_text:
         if(icd_code[0] is not None):
             f.write("__label__" + icd_code[0] + " ")
     f.write(entry[0].replace('\n', '') + '\n')
-            
+   
